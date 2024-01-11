eb38e43723d0f2ac9210d47664e9e2fdfae0252b
arimkim <kal72222222@gmail.com>
2024-01-11 23:23:07 +0900

    Initial commit

---
+++ src/test/java/com/example/cicdtest/StudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cicdtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
