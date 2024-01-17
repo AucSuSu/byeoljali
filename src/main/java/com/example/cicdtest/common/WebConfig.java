2856842bfe9d42af70e6d1bab84b477eac2afaf1
kal72222222 <kal72222222@gmail.com>
2024-01-17 15:45:14 +0900

    [feat] WebConfiguration CORS test(배포 서버 주소로 변경)

--- src/main/java/com/example/cicdtest/common/WebConfig.java
+++ src/main/java/com/example/cicdtest/common/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://54.79.244.100:8080")
+                .allowedOrigins("http://localhost:3000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("Authorization", "Content-Type")
                 .exposedHeaders("Custom-Header")
