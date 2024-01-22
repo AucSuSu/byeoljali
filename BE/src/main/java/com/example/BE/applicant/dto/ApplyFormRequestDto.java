5715e6b71d121492559ef96372a8ef725615f7ef
woomigyeong <migyom222222@gmail.com>
2024-01-22 18:08:46 +0900

    fix: blacklist add api fix

---
+++ BE/src/main/java/com/example/BE/applicant/dto/ApplyFormRequestDto.java
@@ -0,0 +1,14 @@
+package com.example.BE.applicant.dto;
+
+import lombok.Getter;
+
+@Getter
+public class ApplyFormRequestDto {
+
+    private Long memberId;
+    private int boughtAlbum;
+    private Long fanId;
+    private Long artistFansignId;
+
+
+}
--- BE/src/main/java/com/example/BE/applicant/repository/ApplicantRepository.java
+++ BE/src/main/java/com/example/BE/applicant/repository/ApplicantRepository.java
@@ -4,15 +4,22 @@ import com.example.BE.applicant.dto.ApplyPageDto;
 import com.example.BE.applicant.Applicant;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface ApplicantRepository extends JpaRepository<Applicant, Long> {
 
-//    @Query("SELECT af.title, af.posterImageUrl, m.name " +
-//            "FROM MemberFansign mf " +
-//            "JOIN FETCH mf.artistFansign af " +
-//            "JOIN FETCH mf.member m")
-//    List<ApplyPageDto> findApplyPageAllFetchJoin(Long fanId);
+    @Query("SELECT af.title, af.posterImageUrl, m.name " +
+            "FROM MemberFansign mf " +
+            "JOIN FETCH mf.artistFansign af " +
+            "JOIN FETCH mf.member m")
+    List<ApplyPageDto> findAllFetchJoin(Long fanId);
 
+    // 입력받은 artistfansignId 와 memberId로
+    // memberfansignId 찾아주기
+    @Query("select mf.memberfansignId from MemberFansign mf join mf.artistFansign af join mf.member m"
+            + " where af.artistfansignId = :artistfansignId"
+            + " and m.memberId = :memberId")
+    Long asdasdss(@Param("artistfansignId") Long artistfansignId, @Param("memberId") Long memberId);
 }
