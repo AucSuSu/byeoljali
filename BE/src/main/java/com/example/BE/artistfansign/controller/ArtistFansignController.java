195ac5567acaeb1304f0c2a5c88b30af125f2f3b
김아림 <kal72222222@gmail.com>
2024-01-22 14:09:58 +0900

    feat: make fansign api 작성 (post mapping 400 error)

--- BE/src/main/java/com/example/BE/artistfansign/controller/ArtistFansignController.java
+++ BE/src/main/java/com/example/BE/artistfansign/controller/ArtistFansignController.java
@@ -1,12 +1,11 @@
 package com.example.BE.artistfansign.controller;
 
 import com.example.BE.artistfansign.dto.AddArtistFansignRequestDto;
-import com.example.BE.artistfansign.entity.ArtistFansign;
 import com.example.BE.artistfansign.service.ArtistFansignService;
 import com.example.BE.common.HttpStatusEnum;
 import com.example.BE.common.Message;
-import com.example.BE.fan.dto.FanMyPageResponseDto;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,14 +14,16 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 public class ArtistFansignController {
 
     ArtistFansignService artistFansignService;
 
     @PostMapping("/artists/fansign")
     public ResponseEntity<Message> addFansign(@RequestBody AddArtistFansignRequestDto requestDto) {
+        log.info(" ** 팬싸인회 생성 요청 api 입니다.** ");
         Long aritstFansignId = artistFansignService.addFansign(requestDto);
-        Message message = new Message(HttpStatusEnum.OK, "성공", aritstFansignId);
+        Message message = new Message(HttpStatusEnum.OK, "개설 완료", aritstFansignId);
         return new ResponseEntity<>(message, HttpStatus.OK);
     }
 }
