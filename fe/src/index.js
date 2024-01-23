269a24e0f0b2bf5cb48030c7987e7eda8fad772e
RaelJung <allice2283006@gmail.com>
2024-01-23 14:41:09 +0900

    feat: List에 Modal 추가

--- fe/src/index.js
+++ fe/src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import Modal from 'react-modal';
+
+//모달이 열릴 때, App 컴포넌트의 루트 요소를 지정
+Modal.setAppElement('#root');
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
