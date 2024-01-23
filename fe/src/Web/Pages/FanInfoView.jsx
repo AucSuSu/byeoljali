123b3d305f54ce2bead655be56f47a74e62beead
yuntaewoo <allback123@gmail.com>
2024-01-23 17:26:37 +0900

    fix: favicon 수정

---
+++ fe/src/Web/Pages/FanInfoView.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function FanInfoView() {
+  return (
+    <>
+      <h1>FanInfoView</h1>
+      <div>
+        <span>1</span>
+        <span>2</span>
+      </div>
+      <div> 2 </div>
+    </>
+  );
+}
+
+export default FanInfoView;
