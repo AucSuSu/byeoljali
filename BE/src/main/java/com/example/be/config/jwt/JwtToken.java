16678db4bad030c16ed1d42211d51f9f0ecd3b18
TaeHo Yun <ice_98@naver.com>
2024-01-24 15:09:35 +0900

    Feat:소셜로그인 개발(access, refresh 토큰 발급)완료

---
+++ BE/src/main/java/com/example/be/config/jwt/JwtToken.java
@@ -0,0 +1,13 @@
+package com.example.be.config.jwt;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class JwtToken {
+    private String accessToken;
+    private String refreshToken;
+
+
+}
