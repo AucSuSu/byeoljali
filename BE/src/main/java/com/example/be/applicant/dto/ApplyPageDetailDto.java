420ed41fe9807ac5cf7055db0e27f0ff3417d44d
woomigyeong <migyom222222@gmail.com>
2024-01-25 12:47:12 +0900

    feat: 응모페이지 상세 조회 api 완료

--- BE/src/main/java/com/example/be/applicant/dto/ApplyPageDetailDto.java
+++ BE/src/main/java/com/example/be/applicant/dto/ApplyPageDetailDto.java
@@ -13,22 +13,28 @@ public class ApplyPageDetailDto { // 응모 페이지에서 보여야 할 값
     private String artistFansignTitle; // 팬싸인회 이름
     private String memberName; // 해당 멤버 이름
     private String information; // 팬싸인회 정보
-    private LocalDateTime startApplyTime; // 응모 시작 시간
-    private LocalDateTime endApplyTime; // 응모 시작 시간
-    private LocalDateTime startFansignTime;
-    private int orders;
+    private Boolean isWon;
     private FansignStatus status;
 
-    public ApplyPageDetailDto(Long memberfansignId, String posterImageUrl, String artistFansignTitle, String memberName, String information, LocalDateTime startApplyTime, LocalDateTime endApplyTime, LocalDateTime startFansignTime, int orders, FansignStatus status) {
+    // 아래 두 필드: 응모했을 때만(미당첨/당첨자 아직 뽑기 전) 보여주기
+    private LocalDateTime startApplyTime; // 응모 시작 시간
+    private LocalDateTime endApplyTime; // 응모 시작 시간
+
+    // 아래 두 필드: 당첨되었을 때만 보여줌
+    private LocalDateTime startFansignTime; // 팬싸 시작 시간
+    private int orders; // 당첨자의 팬싸 순서 - 미당첨자는 -1로 표기!
+
+    public ApplyPageDetailDto(Long memberfansignId, String posterImageUrl, String artistFansignTitle, String memberName, String information, Boolean isWon, FansignStatus status, LocalDateTime startApplyTime, LocalDateTime endApplyTime, LocalDateTime startFansignTime, int orders) {
         this.memberfansignId = memberfansignId;
         this.posterImageUrl = posterImageUrl;
         this.artistFansignTitle = artistFansignTitle;
         this.memberName = memberName;
         this.information = information;
+        this.isWon = isWon;
+        this.status = status;
         this.startApplyTime = startApplyTime;
         this.endApplyTime = endApplyTime;
         this.startFansignTime = startFansignTime;
         this.orders = orders;
-        this.status = status;
     }
 }
