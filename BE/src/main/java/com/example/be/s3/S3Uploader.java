eeec58d872c263a5e28d08b152619b4d3779a213
TaeHo Yun <ice_98@naver.com>
2024-01-25 16:34:44 +0900

    feat: 팬싸인회 개설시 팬싸인회 포스터 업로드 완료

--- BE/src/main/java/com/example/be/s3/S3Uploader.java
+++ BE/src/main/java/com/example/be/s3/S3Uploader.java
@@ -12,6 +12,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 
@@ -24,9 +25,24 @@ public class S3Uploader {
     @Value("${cloud.aws.s3.bucket}")
     private String bucket;
 
+    // 기본 사진 업로드
     public String upload(MultipartFile multipartFile, String dirName, String name) throws IOException{
         File uploadFile = convert(multipartFile).orElseThrow(() -> new IllegalArgumentException("MultipartFile -> File 전환 실패"));
         return upload(uploadFile, dirName, name);
+    }
+
+    // 팬싸인회 포스터 업로드
+    public String uploadPoster(MultipartFile multipartFile, String dirName, String name, LocalDateTime localDateTime) throws IOException{
+        File uploadFile = convert(multipartFile).orElseThrow(() -> new IllegalArgumentException("MultipartFile -> File 전환 실패"));
+        return uploadPoster(uploadFile, dirName, name, localDateTime);
+    }
+
+    private String uploadPoster(File uploadFile, String dirName, String name, LocalDateTime localDateTime){
+        String fileName = dirName + "/" + name + "/" + uploadFile.getName() + localDateTime.toString();
+        String uploadImageUrl = putS3(uploadFile, fileName);
+        removeNewFile(uploadFile); // MultipartFile -> File 로 전환하며 로컬에 생성된 file 삭제
+
+        return uploadImageUrl;      // 업로드된 파일의 S3 URL 주소 반환
     }
 
     private String upload(File uploadFile, String dirName, String name){
