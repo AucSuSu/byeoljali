7e04854434edcee300e6247632414e673a996e00
RaelJung <allice2283006@gmail.com>
2024-01-25 15:45:39 +0900

    feat: NavBar 추가

--- fe/src/Web/Stores/applyListReducer.js
+++ fe/src/Web/Stores/applyListReducer.js
@@ -4,29 +4,7 @@ import { createSlice } from '@reduxjs/toolkit';
 const applyListSlice = createSlice({
   name: 'applyList',
   initialState: {
-    dataList: [
-      {
-        status: 'OK',
-        message: '팬싸인회',
-        object: [
-          {
-            artistfansignId: 1,
-            title: 'fact check fansign',
-            posterImageUrl: 'https://pbs.twimg.com/media/FZuGhtZaMAEMLdh.jpg',
-            isApplyed: true,
-            status: null,
-          },
-          {
-            artistfansignId: 2,
-            title: 'NCT 영웅팬싸인회',
-            posterImageUrl:
-              'https://i1.sndcdn.com/artworks-HHzS3NMQpYq35RNY-io4p8w-t500x500.jpg',
-            isApplyed: false,
-            status: 'READY_APPLYING',
-          },
-        ],
-      },
-    ],
+    dataList: [],
   },
   reducers: {
     setDataList: (state, action) => {
---
+++ fe/src/Web/Utils/NavBar.jsx
@@ -0,0 +1,90 @@
+// src/Utils/NavBar.js
+
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import ProfileImage from './ProfileImage';
+
+const NavbarContainer = styled.nav`
+  background-color: #888;
+  height: 40px;
+  padding: 10px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const NavbarList = styled.ul`
+  list-style: none;
+  display: flex;
+  gap: 20px;
+`;
+
+const NavbarItem = styled.li`
+  color: #fff;
+  margin-top: 15px;
+`;
+
+const NavbarLink = styled(Link)`
+  text-decoration: none;
+  color: #fff;
+  font-weight: bold;
+
+  &:hover {
+    color: #ffa500;
+  }
+`;
+
+const Navbar = ({ isFan }) => {
+  const profileImageUrl =
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpqYJDnLH7XxH1vrI3VCvNq6E5JipsPi5WrqrlMrWqsA&s';
+
+  return (
+    <NavbarContainer>
+      <h2>Logo</h2>
+      <NavbarList>
+        <NavbarItem>
+          {/* 로고 이미지로 변경 */}
+          <NavbarLink to="/">홈</NavbarLink>
+        </NavbarItem>
+        {/* 팬 */}
+        {isFan && (
+          <>
+            <NavbarItem>
+              <NavbarLink to="/fan-page">응모 내역</NavbarLink>
+            </NavbarItem>
+            <NavbarItem>
+              <NavbarLink to="/fan-events">당첨 내역</NavbarLink>
+            </NavbarItem>
+            <NavbarItem>
+              <NavbarLink to="/fan-events">내 앨범</NavbarLink>
+            </NavbarItem>
+            <NavbarItem>
+              <NavbarLink to="/fan-profile">
+                <ProfileImage imageUrl={profileImageUrl} />
+              </NavbarLink>
+            </NavbarItem>
+          </>
+        )}
+        {/* 아티스트 */}
+        {!isFan && (
+          <>
+            <NavbarItem>
+              <NavbarLink to="/about">응모 관리</NavbarLink>
+            </NavbarItem>
+            <NavbarItem>
+              <NavbarLink to="/contact">팬싸 관리</NavbarLink>
+            </NavbarItem>
+            <NavbarItem>
+              <NavbarLink to="/artist-profile">
+                <ProfileImage imageUrl={profileImageUrl} />
+              </NavbarLink>
+            </NavbarItem>
+          </>
+        )}
+      </NavbarList>
+    </NavbarContainer>
+  );
+};
+
+export default Navbar;
