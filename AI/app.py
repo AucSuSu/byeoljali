9b92161896b207e09091feb85596e4a9c2f0f600
woomigyeong <migyom222222@gmail.com>
2024-01-29 16:28:38 +0900

    feat: flask 서버 공부 및 같은 저장소에 있는 사진들의 distance 확인하는 파일(app.py) 완료

--- AI/app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-
-app=Flask(__name__)
-
-@app.route("/")
-def index():
-    return "Hello\nThis is on Docker"
-
-app.run(port=8080, host="0.0.0.0") # 0.0.0.0을 해야 로컬 호스트와 무관하게 접근 가능
