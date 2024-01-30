24e2cbe44bec3b416d9126ec347012c5c6e34304
yuntaewoo <allback123@gmail.com>
2024-01-30 15:56:54 +0900

    test: 인생네컷

--- fe/src/Web/Pages/FanPhotoView.jsx
+++ fe/src/Web/Pages/FanPhotoView.jsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import FanPhoto from '../Fan/FanPhoto';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+
+import { getUserPhoto } from '../Stores/fanPhotoReducer';
 
 function FanPhotoView() {
   // axios.get으로 유저의 인생네컷을 모두 받았다고 가정\
   const photoData = useSelector((state) => state.fanphoto.data);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getUserPhoto());
+  }, [dispatch]);
 
   if (Array.isArray(photoData)) {
     // photoData is an array, safe to use .map()
