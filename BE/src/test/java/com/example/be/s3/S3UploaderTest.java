9272e83c52a842a4f800c133c7fda9d71f1bdfb8
TaeHo Yun <ice_98@naver.com>
2024-01-30 11:36:39 +0900

    fix: certImage 추가할때 fanRepository flush하게 save 호출해줌

--- BE/src/test/java/com/example/be/s3/S3UploaderTest.java
+++ BE/src/test/java/com/example/be/s3/S3UploaderTest.java
@@ -1,24 +1,24 @@
-package com.example.be.s3;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-
-import java.io.FileNotFoundException;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-@SpringBootTest
-class S3UploaderTest {
-
-    @Autowired
-    S3Uploader s3Uploader;
-
-    @Test
-    public void testDownloadFile() throws FileNotFoundException {
-        String fileName = "arn:aws:s3:::testbyeoljari/artist/NctDream/nctDream.jpg";
-
-        byte[] bytes = s3Uploader.downloadFile(fileName);
-    }
-
-}
+//package com.example.be.s3;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//import java.io.FileNotFoundException;
+//
+//import static org.junit.jupiter.api.Assertions.*;
+//
+//@SpringBootTest
+//class S3UploaderTest {
+//
+//    @Autowired
+//    S3Uploader s3Uploader;
+//
+//    @Test
+//    public void testDownloadFile() throws FileNotFoundException {
+//        String fileName = "arn:aws:s3:::testbyeoljari/artist/NctDream/nctDream.jpg";
+//
+//        byte[] bytes = s3Uploader.downloadFile(fileName);
+//    }
+//
+//}
