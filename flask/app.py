91822a8f1d1d99a5d35affe15214a418bdb471bd
TaeHo Yun <ice_98@naver.com>
2024-02-01 11:21:38 +0900

    feat: 얼굴인식 완료

--- flask/app.py
+++ flask/app.py
@@ -14,8 +14,6 @@ import requests
 from PIL import Image
 from requests_toolbelt import MultipartEncoder
 from werkzeug.serving import run_simple
-import boto3
-from botocore.exceptions import NoCredentialsError
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' # tensorflow의 로깅레벨 설정: error만 보이도록
 app = Flask(__name__)
@@ -35,10 +33,11 @@ def upload_file():
             print("FE에서 FLASK로 헤더와 이미지 전송")
             # fanId 값을 http 헤더에서 추출
             token = request.headers.get('authorization')
-            print("header: " +token)
+            print("token: " +token)
 
             #웹에서 base64로 인코딩된 이미지 정보 가져오기
             one_data = request.files['image'].read()
+            print(type(one_data))
             # _, one_data = request.files['image'].split(',')
 
             print("Success to get incoding image from user") # debugging
@@ -78,14 +77,16 @@ def upload_file():
 
             # 외부 API로부터 데이터 수신할 때
             # : 지정된 url로 post 요청을 보냄
-            response = requests.get(url, headers=headers, data=data).content
-            print(type(response))
-
+            response = requests.get(url, headers=headers).content
+            base64_decoded = base64.b64decode(response)
+            print(type(base64_decoded))
             # 받아온 이미지 데이터를 사용하여 Image.open 호출
-            tmp = io.BytesIO(response)
+            tmp = io.BytesIO(base64_decoded)
             print("tmp common")
             print(type(tmp))
             response_photo = Image.open(tmp)
+
+            
             print("response_photo")
             if response_photo is not None:
                 print("response_photo 잘 저장했습니다")
@@ -93,41 +94,44 @@ def upload_file():
 
             print(type(response_photo)) 
 
-            photo.save("certificate_image.jpg") # 이미지화된 이미지를 check_image_file.jpg로 저장한다
+            response_photo.save("certificate_image.jpg") # 이미지화된 이미지를 check_image_file.jpg로 저장한다
 
 
 
             print("===============================================")
-            # print("저장된 두 사진의 distance 판별 후 전송")
+            print("저장된 두 사진의 distance 판별 후 전송")
 
-            # distance = getDistance()
-            # # 결괏값 출력
-            # pred_value = False
-            # if (distance < 0.6): # 거리가 0.6보다 작을 경우 -> distance 기준 값 변경하기
-            #     pred_value = True # 같은 사람으로 판별
-            # else: # 거리가 더 크면
-            #     pred_value = False # 다른 사람으로 판별
+            distance = getDistance()
             
-            # print("IS SAME PERSON? ",pred_value)
-            # pred_result = {'isSamePerson':pred_value}
+            # 결괏값 출력
+            pred_value = False
+            if (distance < 0.35): # 거리가 0.6보다 작을 경우 -> distance 기준 값 변경하기
+                pred_value = True # 같은 사람으로 판별
+            else: # 거리가 더 크면
+                pred_value = False # 다른 사람으로 판별
+            
+            print("IS SAME PERSON? ",pred_value)
+            pred_result = {'isSamePerson':pred_value}
 
+            print(distance[0])
             # 본인인지 아닌지에 대한 결과값 json 형식으로 보내줌
-            # return jsonify(pred_result) 
-            return jsonify({"please":True})
+            return jsonify(pred_result) 
+            # return jsonify({"please":True})
         except Exception as e:
+            
             print("exception in!")
             return jsonify({"success": False, "message": str(e)})
         
 
         
 def getDistance(): ## 거리를 가져오는 함수
-
     # 들어온 두 값을 비교하여 인코딩한다.
 
     # 저장된 얼굴 사진 - S3에서 certificate_image를 가지고 와야한다!
     certificate_image = fr.load_image_file("./certificate_image.jpg")
-
     #좌표 인식
+    print(certificate_image)
+    print(fr.face_locations(certificate_image))
     top, right, bottom, left = fr.face_locations(certificate_image)[0] # 각 인물의 이미지에서 얼굴의 위치를 찾
     certificate_image = certificate_image[top:bottom, left:right] # 해당 위치를 사용하여 얼굴을 자름
 
@@ -135,7 +139,6 @@ def getDistance(): ## 거리를 가져오는 함수
     unknown_person = fr.load_image_file("./unknown_person.jpg")  
     top, right, bottom, left = fr.face_locations(unknown_person)[0] # 검사 대상 이미지에서 얼굴 위치 찾고
     unknown_face = unknown_person[top:bottom, left:right] # 얼굴만 자르기
-
     enc_u_f = fr.face_encodings(np.ascontiguousarray(unknown_face)) # 검사 대상 이미지의 얼굴 특징 벡터 추출
 
     enc_k_f = fr.face_encodings(np.ascontiguousarray(certificate_image)) # 얼굴 특징 벡터 추출
