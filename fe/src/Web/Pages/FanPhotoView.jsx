8205edfbea7e7f7914a0209b13e49f070c0e3dfa
yuntaewoo <allback123@gmail.com>
2024-02-02 14:07:06 +0900

    fix: useState 수정

--- fe/src/Web/Pages/FanPhotoView.jsx
+++ fe/src/Web/Pages/FanPhotoView.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import FanPhoto from '../Fan/FanPhoto';
 import { useSelector, useDispatch } from 'react-redux';
