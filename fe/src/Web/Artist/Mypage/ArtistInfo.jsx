c5ab3d8018370caf6056ea539e9c0cd131687152
yuntaewoo <allback123@gmail.com>
2024-02-02 15:08:51 +0900

    feat: 아티스트 이미지 수정 기능 생성

--- fe/src/Web/Artist/Mypage/ArtistInfo.jsx
+++ fe/src/Web/Artist/Mypage/ArtistInfo.jsx
@@ -1,9 +1,11 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import MemberList from '../Mypage/MemberList.jsx';
 import AddMemberModal from '../Modal/AddMemberModal.jsx';
 import { useDispatch, useSelector } from 'react-redux';
 import { getInfo } from '../../Stores/artistInfoReducer.js';
 import useAxios from '../../axios.js';
+
+import ArtistImgModal from '../Modal/ArtistImgModal.jsx';
 
 export default function ArtistInfo() {
   const artistData = useSelector((state) => state.artistInfo.artistData);
@@ -21,52 +23,66 @@ export default function ArtistInfo() {
     getArtistInfo();
   }, []);
 
+  // 모달 표시 상태를 관리하는 useState 추가
+  const [showArtistImgModal, setShowArtistImgModal] = useState(false);
+
+  // 이미지 클릭시 모달을 보여주는 함수
+  const handleImageClick = () => {
+    setShowArtistImgModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setShowArtistImgModal(false);
+  };
+
   return (
     <div className="h-screen flex flex-col">
+      <div className="p-8 ml-20 mr-20 overflow-y-auto flex-grow-0">
+        {artistData && (
+          <div className="mb-8 flex items-center">
+            <div className="w-1/2 h-1/2 object-cover rounded-lg mb-8">
+              <img
+                className="w-full h-full object-cover rounded-lg hover:border-4 hover:border-blue-500 transition duration-300"
+                src={artistData.object.artistImageUrl}
+                alt="Artist"
+                onClick={handleImageClick}
+              />
+            </div>
 
-<div className="p-8 ml-20 mr-20 overflow-y-auto flex-grow-0">
-      {artistData && (
-        <div className="mb-8 flex items-center">
-          <div className="w-1/2 h-1/2 object-cover rounded-lg mb-8">
-            <img
-              className="w-full h-full object-cover rounded-lg hover:border-4 hover:border-blue-500 transition duration-300"
-              src={artistData.object.artistImageUrl}
-              alt="Artist"
-            />
-          </div>
-
-          <div className="w-1/2 pl-8 pt-4 pb-4">
-            <div className="mt-4 text-gray-600 border-t-2 border-gray-900">
-              {artistData.object.companyName}
-            </div>
-            <div className="flex items-center justify-between mb-4">
-              <h3 className="text-xl font-semibold mb-4">
-                {artistData.object.name}
-              </h3>
-              <div className="flex space-x-4">
-                <div className="mb-4">icon1</div>
-                <div className="mb-4">icon2</div>
-                <div className="mb-4">icon3</div>
+            <div className="w-1/2 pl-8 pt-4 pb-4">
+              <div className="mt-4 text-gray-600 border-t-2 border-gray-900">
+                {artistData.object.companyName}
               </div>
-            </div>
-            <div className="mt-4 flex justify-center space-x-4 pb-4 border-b border-t border-gray-500">
-              <div className="flex-1 text-center">
-                <p className="py-2 rounded-lg">예정</p>
-                <p className="text-sm text-gray-600 mt-2">12</p>
+              <div className="flex items-center justify-between mb-4">
+                <h3 className="text-xl font-semibold mb-4">
+                  {artistData.object.name}
+                </h3>
+                <div className="flex space-x-4">
+                  <div className="mb-4">icon1</div>
+                  <div className="mb-4">icon2</div>
+                  <div className="mb-4">icon3</div>
+                </div>
               </div>
-              <div className="flex-1 text-center">
-                <p className="py-2 rounded-lg">진행중</p>
-                <p className="text-sm text-gray-600 mt-2 border-l border-r border-gray-300">5</p>
-              </div>
-              <div className="flex-1 text-center">
-                <p className="py-2 rounded-lg">사인회</p>
-                <p className="text-sm text-gray-600 mt-2">3</p>
+              <div className="mt-4 flex justify-center space-x-4 pb-4 border-b border-t border-gray-500">
+                <div className="flex-1 text-center">
+                  <p className="py-2 rounded-lg">예정</p>
+                  <p className="text-sm text-gray-600 mt-2">12</p>
+                </div>
+                <div className="flex-1 text-center">
+                  <p className="py-2 rounded-lg">진행중</p>
+                  <p className="text-sm text-gray-600 mt-2 border-l border-r border-gray-300">
+                    5
+                  </p>
+                </div>
+                <div className="flex-1 text-center">
+                  <p className="py-2 rounded-lg">사인회</p>
+                  <p className="text-sm text-gray-600 mt-2">3</p>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      )}
-    </div>
+        )}
+      </div>
 
       <div className="p-8 overflow-y-auto flex-1">
         <div className="bar flex justify-between items-center mb-4 border-b border-black">
@@ -84,6 +100,13 @@ export default function ArtistInfo() {
           )}
         </div>
       </div>
+      {/* 아티스트 이미지 수정 모달 띄웠다 껐다 하려고 */}
+      {showArtistImgModal && (
+        <ArtistImgModal
+          artistImageUrl={artistData.object.artistImageUrl}
+          onClose={handleCloseModal}
+        />
+      )}
     </div>
   );
 }
