b3027adbb338049360f4f5586ac22aeba223b07a
RaelJung <allice2283006@gmail.com>
2024-02-02 10:13:24 +0900

    fix: NavBar 라우트 수정

--- fe/src/Web/Utils/NavBar.jsx
+++ fe/src/Web/Utils/NavBar.jsx
@@ -68,11 +68,11 @@ const DropdownItem = styled.div`
 
 const Navbar = ({ isFan }) => {
   const dispatch = useDispatch();
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const setLogout = () => {
     dispatch(logout());
-    navigate('/')
+    navigate('/');
   };
 
   const profileImageUrl =
@@ -111,10 +111,10 @@ const Navbar = ({ isFan }) => {
           // Artist일 경우
           <>
             <NavbarItem>
-              <NavbarLink to="/readyfansign">응모 관리</NavbarLink>
+              <NavbarLink to="/fansign">응모 관리</NavbarLink>
             </NavbarItem>
             <NavbarItem>
-              <NavbarLink to="/fansign">팬싸 관리</NavbarLink>
+              <NavbarLink to="/readyfansign">팬싸 관리</NavbarLink>
             </NavbarItem>
             <Dropdown>
               <ProfileImage imageUrl={profileImageUrl} />
