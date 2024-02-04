4ed922e7949ced3a8abb9ce922d2d4c16223614d
yuntaewoo <allback123@gmail.com>
2024-02-04 19:02:00 +0900

    style: 마이페이지 css

--- fe/src/Web/Fan/FanInfoModal.jsx
+++ fe/src/Web/Fan/FanInfoModal.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useRef } from 'react';
 import { useDispatch } from 'react-redux';
 
-import './FanInfoModal.css';
 import useAxios from '../axios';
 
 function FanInfoModal({ userData, onClose }) {
@@ -12,7 +11,7 @@ function FanInfoModal({ userData, onClose }) {
     name: userData.name,
   });
   const [profileImage, setProfileImage] = useState(null);
-  const [previewUrl, setPreviewUrl] = useState(null); // 미리보기 URL을 위한 상태
+  const [previewUrl, setPreviewUrl] = useState(userData.profileImageUrl); // 미리보기 URL을 위한 상태
   const fileInputRef = useRef(); // 파일 입력을 위한 ref
 
   const handleInputChange = (e) => {
@@ -68,47 +67,88 @@ function FanInfoModal({ userData, onClose }) {
   };
 
   return (
-    <div className="modal-background" onClick={handleBackgroundClick}>
-      <div className="modal-content" onClick={handleModalContentClick}>
-        {/* 모달 내용 */}
-        <div className="input-group">
-          <label htmlFor="nickname">Nickname:</label>
-          <input
-            id="nickname"
-            name="nickname"
-            value={localUserData.nickname}
-            onChange={handleInputChange}
-          />
+    <div
+      className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center px-4 py-6 z-50"
+      onClick={handleBackgroundClick} // 여기에 클릭 이벤트 핸들러 추가
+    >
+      <div
+        className="font-milk w-148 h-148 bg-[url('/public/bg.png')] p-6  shadow-lg flex flex-col items-center"
+        onClick={handleModalContentClick} // 모달 내부 클릭 시 이벤트 버블링 방지
+      >
+        <div className="self-start text-4xl font-bold mb-4">
+          정보 수정
+          <div className="mt-1 border-b-2"></div>
         </div>
-        <div className="input-group">
-          <label htmlFor="name">Name:</label>
-          <input
-            id="name"
-            name="name"
-            value={localUserData.name}
-            onChange={handleInputChange}
-          />
+        {/* Profile Image Upload */}
+        <div>
+          <div className="self-start mb-2 font-bold text-xl">프로필 이미지</div>
+          <div className="relative mb-4">
+            <img
+              src={previewUrl || 'path_to_default_image'}
+              alt="Profile"
+              className="w-72 h-60 rounded-lg object-cover"
+            />
+
+            <button
+              onClick={() => fileInputRef.current.click()}
+              className="absolute inset-0 w-full h-full flex items-center justify-center bg-black bg-opacity-50 rounded-lg"
+            >
+              {/* Icon or text to indicate upload, hidden by default and only shown on hover/focus */}
+              <span className="text-white text-5xl">+</span>
+            </button>
+            <input
+              type="file"
+              name="profileImage"
+              onChange={handleImageChange}
+              className="hidden"
+              ref={fileInputRef}
+            />
+          </div>
         </div>
-        <div className="image-upload-container">
-          {previewUrl && (
-            <img src={previewUrl} alt="Preview" className="image-preview" />
-          )}
-          <input
-            type="file"
-            name="profileImage"
-            onChange={handleImageChange}
-            style={{ display: 'none' }} // input을 숨깁니다.
-            ref={fileInputRef}
-          />
-          <button
-            onClick={() => fileInputRef.current.click()}
-            className="upload-button"
-          >
-            이미지 업로드
-          </button>
+
+        <div className="w-72">
+          {/* Input Fields */}
+          <div className="w-full mb-3">
+            <span className="font-bold text-lg">닉네임 </span>
+
+            <input
+              type="text"
+              id="nickname"
+              name="nickname"
+              value={localUserData.nickname}
+              onChange={handleInputChange}
+              className="mt-1 block w-full border-b border-b-1 py-2 px-3 bg-transparent focus:outline-none "
+            />
+          </div>
+          <div className="w-full mb-4">
+            <div className="flex ">
+              <p className="font-bold text-lg w-14 self-center">이름</p>
+              <input
+                type="text"
+                id="name"
+                name="name"
+                value={localUserData.name}
+                onChange={handleInputChange}
+                className="mt-1 block w-full border-b border-b-1  py-2 px-3 bg-transparent focus:outline-none"
+              />
+            </div>
+          </div>
+          {/* Buttons */}
+          <div className="flex w-full">
+            <button
+              onClick={handleSubmit}
+              className="flex-1 py-2 px-4 mr-7 bg-hot-pink text-white font-semibold rounded-md"
+            >
+              수정
+            </button>
+            <button
+              onClick={onClose}
+              className="flex-1 py-2 px-4 bg-light-gray  font-semibold rounded-md "
+            >
+              닫기
+            </button>
+          </div>
         </div>
-        <button onClick={handleSubmit}>저장</button>
-        <button onClick={onClose}>닫기</button>
       </div>
     </div>
   );
