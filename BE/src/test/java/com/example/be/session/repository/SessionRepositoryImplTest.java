15567483201c6f79aae85a776c0f0ea66689d0f0
김아림 <kal72222222@gmail.com>
2024-02-05 16:48:37 +0900

    fix: testCode 주석처리

--- BE/src/test/java/com/example/be/session/repository/SessionRepositoryImplTest.java
+++ BE/src/test/java/com/example/be/session/repository/SessionRepositoryImplTest.java
@@ -20,25 +20,5 @@ class SessionRepositoryImplTest {
 
     @Autowired
     SessionRepository sessionRepository;
-    @Autowired
-    RedisService redisService;
-
-    @Test
-    public void 팬싸인회_세션발급_test(){
-
-        LocalDateTime now = LocalDateTime.now();
-        List<MemberFansign> list =
-        sessionRepository.getMemberFansignList(now);
-
-        for(MemberFansign e : list){
-            System.out.println("sessionId 발급해야할 entity -> " + e.getMemberfansignId());
-        }
-    }
-
-    @Test
-    public void redis에서_세션가져오기_test(){
-        redisService.setValues("1", "key");
-        System.out.println("redis get 1 -> " +   redisService.getValues("1"));
-    }
 
 }
