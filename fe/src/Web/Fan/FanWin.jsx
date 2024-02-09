355d6f605546599f4a6e423c87d6f403492ac484
yuntaewoo <allback123@gmail.com>
2024-02-10 03:07:10 +0900

    fix: fanwin css

--- fe/src/Web/Fan/FanWin.jsx
+++ fe/src/Web/Fan/FanWin.jsx
@@ -23,24 +23,28 @@ function FanWin() {
   };
 
   return (
-    <div className="w-[80%] ml-[10%]">
-      <div className="flex items-center justify-between pb-12 mb-6">
-        <div>
-          <div className="text-3xl bolder mb-2 text-white">당첨 내역</div>
-          <div className="text-white">
-            {data.length} 개의 당첨 내역을 보유 하고 있습니다.
-          </div>
-          <div className="pt-2 text-white">
-            팬싸인회는 시작 30분 전부터 입장 가능합니다
+    <>
+      <div className="w-[80%] ml-[10%]">
+        <div className="flex items-center justify-between pb-12 mb-6">
+          <div>
+            <div className="text-3xl bolder mb-2 text-white">당첨 내역</div>
+            <div className="text-dark-gray">
+              {data.length} 개의 당첨 내역을 보유 하고 있습니다.
+            </div>
+            <div className="pt-2 text-dark-gray">
+              팬싸인회는 시작 30분 전부터 입장 가능합니다
+            </div>
           </div>
         </div>
       </div>
-      <div className="w-full grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2 p-4 border-2 border-white">
-        {data.map((data, index) => (
-          <FanSignList key={index} data={data} />
-        ))}
+      <div className="w-[86%] ml-[7%]">
+        <div className="w-full grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2 p-4 border-2 border-dark-gray rounded-md">
+          {data.map((data, index) => (
+            <FanSignList key={index} data={data} />
+          ))}
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
