01d0ebec2d79ad9a3575ad1f6eb26c55068d1650
woomigyeong <migongeda@naver.com>
2024-02-09 15:27:01 +0900

    feat: 메인 페이지에서 모든 아티스트 리스트 조회 api 완료

--- BE/src/main/java/com/example/be/artist/repository/ArtistRepository.java
+++ BE/src/main/java/com/example/be/artist/repository/ArtistRepository.java
@@ -1,14 +1,20 @@
 package com.example.be.artist.repository;
 
+import com.example.be.artist.dto.ArtistsResponseDto;
 import com.example.be.artist.entity.Artist;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
-@Repository
 public interface ArtistRepository extends JpaRepository<Artist, Long> {
 
 
     Optional<Artist> findByEmail(String email);
+
+    @Query("select new com.example.be.artist.dto.ArtistsResponseDto(artistId, name, artistImageUrl) from Artist")
+    List<ArtistsResponseDto> findAllArtists();
+
 }
