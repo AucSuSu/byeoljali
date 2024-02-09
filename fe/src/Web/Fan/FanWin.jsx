62fadc0525d44703555f6d3f804d9e32dc7b4509
yuntaewoo <allback123@gmail.com>
2024-02-10 02:17:25 +0900

    test: 내 당첨 css

--- fe/src/Web/Fan/FanWin.jsx
+++ fe/src/Web/Fan/FanWin.jsx
@@ -23,17 +23,19 @@ function FanWin() {
   };
 
   return (
-    <div className="w-[80%] m-auto">
+    <div className="w-[80%]">
       <div className="flex items-center justify-between pb-12 mb-6">
         <div>
-          <div className="text-3xl bolder mb-2">당첨 내역</div>
-          <div>{data.length} 개의 당첨 내역을 보유 하고 있습니다.</div>
-          <div className="pt-2">
+          <div className="text-3xl bolder mb-2 text-white">당첨 내역</div>
+          <div className="text-white">
+            {data.length} 개의 당첨 내역을 보유 하고 있습니다.
+          </div>
+          <div className="pt-2 text-white">
             팬싸인회는 시작 30분 전부터 입장 가능합니다
           </div>
         </div>
       </div>
-      <div className="w-[80%] m-auto grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 pd-4 border-2 border-white">
+      <div className="w-[80%] grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 pd-4 border-2 border-white">
         {data.map((data, index) => (
           <FanSignList key={index} data={data} />
         ))}
