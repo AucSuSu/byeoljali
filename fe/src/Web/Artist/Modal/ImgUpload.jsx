cfc55116d1d8d2c068196663e7f4a709eff27956
Munsoo Kang <dalsu1742@gmail.com>
2024-02-09 19:58:32 +0900

    feat:home 타이머 추가

--- fe/src/Web/Artist/Modal/ImgUpload.jsx
+++ fe/src/Web/Artist/Modal/ImgUpload.jsx
@@ -39,15 +39,20 @@ export default function ImgUpload({ img, uploadImg }) {
   };
 
   return (
-    <div>
+    <>
       <div className="flex justify-center">
-        {imageSrc && (
+        {imageSrc ? (
           <img
             id="image-preview"
-            className="rounded-xl border-2 border-pink"
+            className=" w-[200px] h-[200px] object-cover rounded-xl border-2 border-pink"
             src={imageSrc}
             alt="Preview"
           />
+        ) : (
+          <div className="w-[200px] h-[200px] bg-green-500">
+            {' '}
+            커버 이미지 추가
+          </div>
         )}
         <input
           type="file"
@@ -60,8 +65,8 @@ export default function ImgUpload({ img, uploadImg }) {
         className="bg-pink text-black px-4 py-3 rounded-xl mx-auto block mt-4"
         onClick={handleButtonClick}
       >
-        커버 이미지 추가
+        이미지 업로드하기
       </button>
-    </div>
+    </>
   );
 }
