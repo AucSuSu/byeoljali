f9fc61a31d5e1417942d9e9cc34cd82e3ff3bf04
yuntaewoo <allback123@gmail.com>
2024-02-12 00:41:27 +0900

    test: 응모중 css

--- fe/src/Web/Fan/FanSignList.jsx
+++ fe/src/Web/Fan/FanSignList.jsx
@@ -10,21 +10,55 @@ function FanSignList({ data }) {
 
   return (
     <>
-      <div className="text-white bg-slate-900 rounded-md">
-        <div className="w-[80%] ml-[10%]">
-          <img
-            onClick={toggleModal}
-            src={data.posterImageUrl}
-            alt="Poster Image"
-            className="w-full h-auto aspect-square  cursor-pointer hover:scale-105 transition-transform ease-in-out duration-500 mt-8 mb-2"
-          />
-          <div>{data.artistFansignTitle}</div>
-        </div>
+      {data.fanSignStatus === 'APPLYING' ? (
+        // 응모내역
+        <div className="text-white bg-slate-900 rounded-md">
+          <div className="w-[80%] ml-[10%]">
+            <img
+              onClick={toggleModal}
+              src={data.posterImageUrl}
+              alt="Poster Image"
+              className="w-full h-auto aspect-square  cursor-pointer hover:scale-105 transition-transform ease-in-out duration-500 mt-8 mb-2"
+            />
+            <div className="flex">
+              <div className="border-1 border-rounded-md border-kakao-yellow">
+                APPLYING
+              </div>
+              {data.mode === 'RANDOM' ? (
+                <div className="border-1 border-rounded-md border-sky-blue">
+                  RANDOM
+                </div>
+              ) : (
+                <div className="border-1 border-rounded-md border-green-900">
+                  LINE
+                </div>
+              )}
+            </div>
+            <div>{data.artistFansignTitle}</div>
+          </div>
 
-        {isModalVisible && (
-          <FanSignModal data={data} onClose={() => setModalVisible(false)} />
-        )}
-      </div>
+          {isModalVisible && (
+            <FanSignModal data={data} onClose={() => setModalVisible(false)} />
+          )}
+        </div>
+      ) : (
+        // 당첨내역
+        <div className="text-white bg-slate-900 rounded-md">
+          <div className="w-[80%] ml-[10%]">
+            <img
+              onClick={toggleModal}
+              src={data.posterImageUrl}
+              alt="Poster Image"
+              className="w-full h-auto aspect-square  cursor-pointer hover:scale-105 transition-transform ease-in-out duration-500 mt-8 mb-2"
+            />
+            <div>{data.artistFansignTitle}</div>
+          </div>
+
+          {isModalVisible && (
+            <FanSignModal data={data} onClose={() => setModalVisible(false)} />
+          )}
+        </div>
+      )}
     </>
   );
 }
