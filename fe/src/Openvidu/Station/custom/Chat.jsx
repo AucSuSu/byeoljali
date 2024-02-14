570a4a35afc84dcd19f68adcc789f8e15054c618
Munsoo Kang <dalsu1742@gmail.com>
2024-02-14 15:50:16 +0900

    fix:openvidu

--- fe/src/Openvidu/Station/custom/Chat.jsx
+++ fe/src/Openvidu/Station/custom/Chat.jsx
@@ -54,7 +54,7 @@ const Chat = ({ messages, handleSendMessage, orders }) => {
                 </div>
                 <p className="font-bold">{messageData.nickname}</p>
               </div>
-              <p className="border-1 border-dark-gray text-right rounded-lg font-jamsil text-sm w-[60%] ml-auto pl-3 mb-3 ">
+              <p className="border-1 border-dark-gray text-left rounded-lg font-jamsil text-sm w-[60%] ml-auto pl-3 mb-3 ">
                 {messageData.text}
               </p>
             </div>
