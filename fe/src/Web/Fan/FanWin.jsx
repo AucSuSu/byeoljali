ffaadd76fea186031c50f7e846b53779be2aec82
김아림 <kal72222222@gmail.com>
2024-02-14 11:31:10 +0900

    feat: 빈 응모, 당첨에 별가루, 별똥별 뿌리기

--- fe/src/Web/Fan/FanWin.jsx
+++ fe/src/Web/Fan/FanWin.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { loadWin } from '../Stores/fanApplyListReducer';
 import { useNavigate } from 'react-router-dom';
@@ -6,13 +6,32 @@ import FanSignList from '../Fan/FanSignList';
 import useAxios from '../axios';
 
 function FanWin() {
+  const [stars, setStars] = useState([]);
+
   const customAxios = useAxios();
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const data = useSelector((state) => state.fanapply.data);
   console.log(data);
+  const getRandomNumber = (min, max) => {
+    return Math.random() * (max - min) + min;
+  };
 
   useEffect(() => {
+    const makeStars = () => {
+      const numStars = 50; // 원하는 별의 개수
+
+      const newStars = Array.from({ length: numStars }, (_, index) => ({
+        id: index,
+        left: Math.random() * 100 + 'vw', // 랜덤한 가로 위치
+        top: Math.random() * 100 + 'vh', // 랜덤한 세로 위치
+      }));
+
+      setStars(newStars);
+    };
+
+    makeStars();
+
     loadWinData();
   }, []);
 
@@ -45,15 +64,42 @@ function FanWin() {
       </div>
       <div className="w-[86%] ml-[7%] mb-5">
         {data.length === 0 ? (
-          <div className="flex flex-col justify-center items-center px-16 py-32 border-4 border-deep-dark rounded-lg text-white font-jamsil text-35">
-            <div>당첨 내역이 없습니다</div>
-            <div
-              className="text-25 text-hot-pink cursor-pointer hover:scale-110 transition-transform ease-in-out duration-500"
-              onClick={goToHome}
-            >
-              응모 하러 가기
+          <>
+            <div className="night z-10 absolute top-0 left-0 right-0 bottom-0 ;">
+            {[...Array(5)].map((_, index) => (
+              <div
+                className="shooting_star"
+                key={index}
+                style={{
+                  top: `${getRandomNumber(0, 100)}%`,
+                  left: `${getRandomNumber(0, 100)}%`,
+                  animationDelay: `-${getRandomNumber(0, 100)}ms`,
+                }}
+              ></div>
+            ))}
+                    </div>
+                    <div>
+            {stars.map((star) => (
+                <div
+                  key={star.id}
+                  className="star"
+                  style={{
+                    left: star.left,
+                    top: star.top,
+                  }}
+                ></div>
+              ))}
+                    </div>
+            <div className="flex flex-col justify-center items-center px-16 py-32 border-4 border-deep-dark rounded-lg text-white font-jamsil text-35">
+              <div>당첨 내역이 없습니다</div>
+              <div
+                className="text-25 text-hot-pink cursor-pointer hover:scale-110 transition-transform ease-in-out duration-500"
+                onClick={goToHome}
+              >
+                응모 하러 가기
+              </div>
             </div>
-          </div>
+          </>
         ) : (
           <>
             <div className="w-full grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-8 border-4 border-deep-dark rounded-lg mb-8">
