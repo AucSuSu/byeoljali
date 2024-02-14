d4116d70bd449acf0d7851b1bc4b4ffa202953a3
yuntaewoo <allback123@gmail.com>
2024-02-14 19:41:42 +0900

    fix: 아티스트 팬싸 없을 시 나오는 화면

--- fe/src/Web/Stores/artistFansignReducer.js
+++ fe/src/Web/Stores/artistFansignReducer.js
@@ -3,7 +3,7 @@ import { createSlice } from '@reduxjs/toolkit';
 const artistFansignSlice = createSlice({
   name: 'artistFansign',
   initialState: {
-    data: null,
+    data: { object: [] },
     connectedData: null,
     detail: null,
     status: 'idle',
