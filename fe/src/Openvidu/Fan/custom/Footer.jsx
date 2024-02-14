13fe93fee7f00dd622ac0559ed67bf10d7c4473d
yuntaewoo <allback123@gmail.com>
2024-02-14 15:37:19 +0900

    fix: border top px

--- fe/src/Openvidu/Fan/custom/Footer.jsx
+++ fe/src/Openvidu/Fan/custom/Footer.jsx
@@ -31,7 +31,7 @@ export default function Footer({
     if (count > 0) {
       captureArea();
       const captureBounds = document.querySelector('.bounds');
-      captureBounds.style.borderTop = '13px solid white';
+      captureBounds.style.borderTop = '30px solid white';
       captureBounds.style.borderBottom = '8px solid white';
       captureBounds.style.borderRight = '5px solid white';
       captureBounds.style.borderLeft = '5px solid white';
