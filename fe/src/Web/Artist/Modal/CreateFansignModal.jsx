2dd6e6311ba6b2a50d636ca54b5e26a7945829f1
yuntaewoo <allback123@gmail.com>
2024-02-14 17:25:20 +0900

    fix: 팬싸 생성 모달 수정

--- fe/src/Web/Artist/Modal/CreateFansignModal.jsx
+++ fe/src/Web/Artist/Modal/CreateFansignModal.jsx
@@ -61,26 +61,26 @@ export default function CreateFansignModal({}) {
         Swal.fire({
           icon: 'success',
           title: '성공적으로 개설되었습니다.',
-          background : '#222222',
-          confirmButtonColor: "#FF2990",   
-          confirmButtonText: "OK",
-          borderRadius : "30px"
+          background: '#222222',
+          confirmButtonColor: '#FF2990',
+          confirmButtonText: 'OK',
+          borderRadius: '30px',
         }).then((result) => {
-          if( result.isConfirmed){
+          if (result.isConfirmed) {
             closeModal();
           }
-        }
-        );
+        });
         return res.data;
-      }).catch((error) => {
+      })
+      .catch((error) => {
         if (error.response && error.response.status === 413) {
           Swal.fire({
             icon: 'warning',
             title: '이미지 크기를 1mb 보다 낮춰주세요',
-            background : '#222222',
-            confirmButtonColor: "#FF2990",   
-            confirmButtonText: "OK",
-          })
+            background: '#222222',
+            confirmButtonColor: '#FF2990',
+            confirmButtonText: 'OK',
+          });
         }
       });
   };
@@ -176,9 +176,7 @@ export default function CreateFansignModal({}) {
         contentLabel="테스트입니다"
         style={customStyle}
       >
-        <div
-          className="flex font-jamsil bg-black overflow-hidden ps-3 pt-5"
-        >
+        <div className="flex font-jamsil bg-black overflow-hidden ps-3 pt-5">
           {stars.map((star) => (
             <div
               key={star.id}
@@ -190,176 +188,161 @@ export default function CreateFansignModal({}) {
               }}
             ></div>
           ))}
-          
-          <div className="w-1/2 flex flex-col justify-center items-align">
-          <div className="flex  justify-center items-align">
-                  <input
-                    type="text"
-                    value={title}
-                    placeholder='팬싸인회 타이틀'
-                    className="placeholder-hot-pink border-b focus:border-hot-pink outline-none w-120 text-black p-3 rounded-xl mb-3"
-                    onChange={(e) => setTitle(e.target.value)}
-                  />
-                </div>
-            <ImgUpload img={null} uploadImg={uploadImg} 
-            className ="ml-10"/>
-          </div>
-          <div className="w-1/2"
-          style={{
-            position : 'relative'
-          }}>
-            <div className="font-jamsil pl-3">
-                
-                <div className="pt-5 flex flex-col">
-                  <label className="text-hot-pink pb-1 text-25">
-                    공지사항{' '}
-                  </label>
-                  <CKEditor
-                    editor={ClassicEditor}
-                    data={information}
-                    config={{
-                      // 이미지 업로드 비활성화
-                      toolbar: [
-                        'heading',
-                        '|',
-                        'bold',
-                        'italic',
-                        'link',
-                        'bulletedList',
-                      ],
-                      placeholder: '공지사항을 입력해주세요!',
-                      editorStyles: {
-                        color: 'black', // 텍스트 색상을 검정색으로 지정
-                      },
-                    }}
-                    onReady={(editor) => {
-                      console.log('Editor is ready to use!', editor);
-                    }}
-                    onChange={(event, editor) => {
-                      const newData = editor.getData();
-                      setInformation(newData);
-                    }}
-                    onBlur={(event, editor) => {
-                      console.log('Blur.', editor);
-                    }}
-                    onFocus={(event, editor) => {
-                      console.log('Focus.', editor);
-                    }}
-                  />
-                </div>
-                <div className="mt-14 flex flex-col">
-                  <label className="text-hot-pink pb-1 ">앨범명</label>
-                  <input
-                    type="text"
-                    value={albumName}
-                    className="border-b border-gray-300 focus:border-hot-pink outline-none w-120 text-black p-1  rounded-xl p-1"
-                    onChange={(e) => setAlbumName(e.target.value)}
-                  />
-                </div>
-                <div className="mt-4 flex items-center">
-                  <label className="text-hot-pink">응모시작 </label>
-                  <input
-                    type="date"
-                    className="ml-3 text-black rounded-xl p-1"
-                    min={startApplyTime}
-                    value={startApplyTime}
-                    onChange={(e) => setStartApplyTime(e.target.value)}
-                  />
-                  <label className="text-hot-pink ms-3">응모마감 </label>
-                  <input
-                    type="date"
-                    className="ml-3 text-black rounded-xl p-1"
-                    min={startApplyTime}
-                    value={endApplyTime}
-                    onChange={(e) => setEndApplyTime(e.target.value)}
-                  />
-                </div>
-                <div className="mt-4 flex items-center">
-                  <label className="text-hot-pink">응모방식 </label>
-                  <div className="ml-4 text-white">
-                    <label htmlFor="modeRandom">Random</label>
-                    <input
-                      type="checkbox"
-                      id="modeRandom"
-                      className="ml-3 text-black"
-                      checked={mode.random}
-                      onChange={() => {
-                        handleModeCheck('RANDOM');
-                      }}
-                    />
-                    <label className="ml-4" htmlFor="modeLine">
-                      Line
-                    </label>
-                    <input
-                      type="checkbox"
-                      id="modeLine"
-                      className="ml-3 text-black"
-                      checked={mode.line}
-                      onChange={() => {
-                        handleModeCheck('DESC');
-                      }}
-                    />
-                  </div>
-                </div>
-                <div className="mt-2 mb-5 flex items-center">
-                  <label className="text-hot-pink">팬싸인회 시작 시간</label>
-                  <input
-                    type="date"
-                    className="ml-3 text-black rounded-xl p-1"
-                    min={endApplyTime}
-                    value={startFansignDate}
-                    onChange={(e) => setStartFansignDate(e.target.value)}
-                  />
-                  <select
-                    value={startFansignHour}
-                    className="ml-3 text-black rounded-xl p-1"
-                    onChange={(e) => setStartFansignHour(e.target.value)}
-                  >
-                    {[...Array(24).keys()].map((hour) => (
-                      <option key={hour} value={String(hour).padStart(2, '0')}>
-                        {String(hour).padStart(2, '0')}:00
-                      </option>
-                    ))}
-                  </select>
-                </div>
 
-                <p className="text-18 text-center text-hot-pink">개설 멤버</p>
-                <div className="text-white bg-pink px-4 py-4 rounded-xl  grid grid-cols-4 gap-4">
-                  {Object.keys(members).map((member) => (
-                    <div key={member}>
-                      <input
-                        type="checkbox"
-                        id={member}
-                        checked={selectedMembers.includes(member)}
-                        onChange={() => handleMemberCheck(member)}
-                      />
-                      <label className="ml-2">{member}</label>
-                    </div>
+          <div className="w-1/2 pt-[2.5%] h-[90%] flex flex-col justify-between items-align">
+            <div className="flex  justify-center items-align">
+              <input
+                type="text"
+                value={title}
+                placeholder="팬싸인회 타이틀"
+                className="placeholder-hot-pink border-b focus:border-hot-pink outline-none w-120 text-black p-3 rounded-xl mb-3"
+                onChange={(e) => setTitle(e.target.value)}
+              />
+            </div>
+            <ImgUpload img={null} uploadImg={uploadImg} className="ml-10" />
+          </div>
+          <div className="w-1/2 h-[auto]">
+            <div className="font-jamsil pl-3">
+              <div className="pt-5 flex flex-col">
+                <label className="text-hot-pink pb-1 text-25">공지사항 </label>
+                <CKEditor
+                  editor={ClassicEditor}
+                  data={information}
+                  config={{
+                    // 이미지 업로드 비활성화
+                    toolbar: [
+                      'heading',
+                      '|',
+                      'bold',
+                      'italic',
+                      'link',
+                      'bulletedList',
+                    ],
+                    placeholder: '공지사항을 입력해주세요!',
+                    editorStyles: {
+                      color: 'black', // 텍스트 색상을 검정색으로 지정
+                    },
+                  }}
+                  onReady={(editor) => {
+                    console.log('Editor is ready to use!', editor);
+                  }}
+                  onChange={(event, editor) => {
+                    const newData = editor.getData();
+                    setInformation(newData);
+                  }}
+                  onBlur={(event, editor) => {
+                    console.log('Blur.', editor);
+                  }}
+                  onFocus={(event, editor) => {
+                    console.log('Focus.', editor);
+                  }}
+                />
+              </div>
+              <div className="mt-14 flex flex-col">
+                <label className="text-hot-pink pb-1 ">앨범명</label>
+                <input
+                  type="text"
+                  value={albumName}
+                  className="border-b border-gray-300 focus:border-hot-pink outline-none w-120 text-black p-1  rounded-xl p-1"
+                  onChange={(e) => setAlbumName(e.target.value)}
+                />
+              </div>
+              <div className="mt-4 flex items-center">
+                <label className="text-hot-pink">응모시작 </label>
+                <input
+                  type="date"
+                  className="ml-3 text-black rounded-xl p-1"
+                  min={startApplyTime}
+                  value={startApplyTime}
+                  onChange={(e) => setStartApplyTime(e.target.value)}
+                />
+                <label className="text-hot-pink ms-3">응모마감 </label>
+                <input
+                  type="date"
+                  className="ml-3 text-black rounded-xl p-1"
+                  min={startApplyTime}
+                  value={endApplyTime}
+                  onChange={(e) => setEndApplyTime(e.target.value)}
+                />
+              </div>
+              <div className="mt-4 flex items-center">
+                <label className="text-hot-pink">응모방식 </label>
+                <div className="ml-4 text-white">
+                  <label htmlFor="modeRandom">Random</label>
+                  <input
+                    type="checkbox"
+                    id="modeRandom"
+                    className="ml-3 text-black"
+                    checked={mode.random}
+                    onChange={() => {
+                      handleModeCheck('RANDOM');
+                    }}
+                  />
+                  <label className="ml-4" htmlFor="modeLine">
+                    Line
+                  </label>
+                  <input
+                    type="checkbox"
+                    id="modeLine"
+                    className="ml-3 text-black"
+                    checked={mode.line}
+                    onChange={() => {
+                      handleModeCheck('DESC');
+                    }}
+                  />
+                </div>
+              </div>
+              <div className="mt-2 mb-5 flex items-center">
+                <label className="text-hot-pink">팬싸인회 시작 시간</label>
+                <input
+                  type="date"
+                  className="ml-3 text-black rounded-xl p-1"
+                  min={endApplyTime}
+                  value={startFansignDate}
+                  onChange={(e) => setStartFansignDate(e.target.value)}
+                />
+                <select
+                  value={startFansignHour}
+                  className="ml-3 text-black rounded-xl p-1"
+                  onChange={(e) => setStartFansignHour(e.target.value)}
+                >
+                  {[...Array(24).keys()].map((hour) => (
+                    <option key={hour} value={String(hour).padStart(2, '0')}>
+                      {String(hour).padStart(2, '0')}:00
+                    </option>
                   ))}
-                </div>
-                <div className="flex justify-center pb-3"
-                style={
-                  { 
-                  position : 'absolute', 
-                  bottom : '0',
-                  left: '50%',
-                  transform: 'translateX(-50%)'
-              }
-                }>
-                  <button
-                    type="submit"
-                    className="bg-hot-pink text-black px-4 py-2 rounded-xl"
-                    onClick={fansignCreate}
-                  >
-                    개설하기
-                  </button>
-                  <button
-                    className="bg-light-gray text-black px-3 py-1 rounded-xl ml-5"
-                    onClick={closeModal}
-                  >
-                    닫기
-                  </button>
-                </div>
+                </select>
+              </div>
+
+              <p className="text-18 text-center text-hot-pink">개설 멤버</p>
+              <div className="text-white bg-pink px-4 py-4 rounded-xl  grid grid-cols-4 gap-4">
+                {Object.keys(members).map((member) => (
+                  <div key={member}>
+                    <input
+                      type="checkbox"
+                      id={member}
+                      checked={selectedMembers.includes(member)}
+                      onChange={() => handleMemberCheck(member)}
+                    />
+                    <label className="ml-2">{member}</label>
+                  </div>
+                ))}
+              </div>
+              <div className="flex justify-center pb-3">
+                <button
+                  type="submit"
+                  className="bg-hot-pink text-black px-4 py-2 rounded-xl"
+                  onClick={fansignCreate}
+                >
+                  개설하기
+                </button>
+                <button
+                  className="bg-light-gray text-black px-3 py-1 rounded-xl ml-5"
+                  onClick={closeModal}
+                >
+                  닫기
+                </button>
+              </div>
             </div>
           </div>
         </div>
