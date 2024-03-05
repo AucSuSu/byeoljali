2f16d21371fc520ca23a3be5247573e157d6f34f
Taeho <ice_98@naver.com>
2024-03-05 22:43:44 +0900

    fix: accessToken 만료시간 설정

--- BE/src/main/java/com/example/be/config/jwt/JwtProperties.java
+++ BE/src/main/java/com/example/be/config/jwt/JwtProperties.java
@@ -4,7 +4,7 @@ public interface JwtProperties {
     String ACCESS_TOKEN = "Access";
     String REFRESH_TOKEN = "Refresh";
     String SECRET = "secretKey"; // 암호화 할 비밀키
-    int ACCESS_EXPIRATION_TIME = 100000000; // 2주 (1/1000초)
+    int ACCESS_EXPIRATION_TIME = 1000*60*30; // 30분 (1/1000초)
     int REFRESH_EXPIRATION_TIME = 100000000; // 2주
 
     String REDIS_REFRESH_PREFIX = "REFRESH_TOKEN_";
